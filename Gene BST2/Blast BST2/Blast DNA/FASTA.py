1cd4d13c2a8b59571862fc8e548a82f858a337a3
pg42862 <74066156+pg42862@users.noreply.github.com>
2021-01-31 21:51:04 +0000

    er

---
+++ Gene BST2/Blast BST2/Blast DNA/FASTA.py
@@ -0,0 +1,21 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+
+
+print('A criar ficheiro...')
+Output = 'BST2homologos.fasta'
+ficheiro_output = open(Output, 'w+')
+ficheiro = open('id_listDNA_BST2.txt', 'r')
+b_file = ficheiro.readlines()
+print('A recolher informações...')
+for a in b_file:
+    Entrez.email = 'example@gmail.com'
+    File = Entrez.efetch(db= 'Nucleotide', id= a, retmode='text', rettype= 'fasta')
+    print(File)
+    read = SeqIO.read(File, 'fasta')
+    File.close()
+    ficheiro_output.write('>' + a + str(read.seq) + '\n' + '\n')
+    print('Ficheiro gravado sobre o nome ' + Output)
+
+
